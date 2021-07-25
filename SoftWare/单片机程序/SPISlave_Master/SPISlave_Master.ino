259a41bfd4a3e83bb7839ea1773682eaac7b0bd0
DESKTOP-16QONQE\cmy <cmy_job@163.com>
2021-07-25 21:04:29 +0800

    主程序测试

--- SoftWare/单片机程序/SPISlave_Master/SPISlave_Master.ino
+++ SoftWare/单片机程序/SPISlave_Master/SPISlave_Master.ino
@@ -27,53 +27,13 @@ class ESPMaster {
       digitalWrite(_ss_pin, HIGH);
     }
 
-    uint32_t readStatus() {
-      digitalWrite(_ss_pin, LOW);
-      SPI.transfer(0x04);
-      uint32_t status = (SPI.transfer(0) | ((uint32_t)(SPI.transfer(0)) << 8) | ((uint32_t)(SPI.transfer(0)) << 16) | ((uint32_t)(SPI.transfer(0)) << 24));
-      digitalWrite(_ss_pin, HIGH);
-      return status;
-    }
-
-    void writeStatus(uint32_t status) {
-      digitalWrite(_ss_pin, LOW);
-      SPI.transfer(0x01);
-      SPI.transfer(status & 0xFF);
-      SPI.transfer((status >> 8) & 0xFF);
-      SPI.transfer((status >> 16) & 0xFF);
-      SPI.transfer((status >> 24) & 0xFF);
-      digitalWrite(_ss_pin, HIGH);
-    }
-
-    void readData(uint8_t * data) {
-      digitalWrite(_ss_pin, LOW);
-      SPI.transfer(0x03);
-      SPI.transfer(0x00);
-      for (uint8_t i = 0; i < 32; i++) {
-        data[i] = SPI.transfer(0);
-      }
-      digitalWrite(_ss_pin, HIGH);
-    }
-
     void writeData(uint8_t * data, size_t len) {
       uint8_t i = 0;
       digitalWrite(_ss_pin, LOW);
-      SPI.transfer(0x02);
-      SPI.transfer(0x00);
       while (len-- && i < 32) {
         SPI.transfer(data[i++]);
       }
-      while (i++ < 32) {
-        SPI.transfer(0);
-      }
       digitalWrite(_ss_pin, HIGH);
-    }
-
-    String readData() {
-      char data[33];
-      data[32] = 0;
-      readData((uint8_t *)data);
-      return String(data);
     }
 
     void writeData(const char * data) {
@@ -83,14 +43,13 @@ class ESPMaster {
 
 ESPMaster esp(SS);
 
+
 void send(const char * message) {
+  uint8_t _send_data = 0x55;
   Serial.print("Master: ");
   Serial.println(message);
   esp.writeData(message);
   delay(10);
-  Serial.print("Slave: ");
-  Serial.println(esp.readData());
-  Serial.println();
 }
 
 void setup() {
@@ -102,6 +61,8 @@ void setup() {
 }
 
 void loop() {
+  static uint8_t  test=0x55;
   delay(1000);
-  send("Are you alive?");
+  send((char*)&test);
+  test++;
 }
