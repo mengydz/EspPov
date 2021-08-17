bd87cf10b1833828e93ba20ccec4ad865f3841e3
mengydz <472283272@qq.com>
2021-08-17 22:05:08 +0800

    流水灯

--- SoftWare/单片机程序/SPISlave_Master/SPISlave_Master.ino
+++ SoftWare/单片机程序/SPISlave_Master/SPISlave_Master.ino
@@ -35,34 +35,48 @@ class ESPMaster {
       }
       digitalWrite(_ss_pin, HIGH);
     }
-
-    void writeData(const char * data) {
-      writeData((uint8_t *)data, strlen(data));
-    }
 };
 
 ESPMaster esp(SS);
 
-
-void send(const char * message) {
-  uint8_t _send_data = 0x55;
-  Serial.print("Master: ");
-  Serial.println(message);
-  esp.writeData(message);
-  delay(10);
+#define ELEMCNT 16 //定义数组元素个数
+uint8_t a[ELEMCNT]={0}; //被操作的元素
+uint8_t bb[ELEMCNT]={0};
+uint8_t exceedA=0,exceedB=0;//记忆溢出位用的变量，其中exceedA为上一元素溢出值，exceedB为本元素溢出值
+void LeftElem(int k)
+{
+  int i;  
+  uint8_t Msk=0; //用于计算溢出位的掩码，如k=6时，其掩码值为00000011(二进制)  
+  for (i=0;i<8-k;i++)
+  {    
+    Msk=Msk|(1<<i);  
+  }   
+  //移动所有数组元素,i值对应数组元素下标i-1
+  for (i=0;i<ELEMCNT;i++)
+  {    
+    exceedB=(a[i]&(~Msk))>>(8-k);//计算溢出数据  
+    exceedB = a[i]>>7;
+    a[i]=a[i]<<1;    
+    a[i]+=exceedA;
+    exceedA=exceedB;  
+  }
 }
+
 
 void setup() {
   Serial.begin(115200);
   SPI.begin();
   esp.begin();
+  a[0] = 0x01;
   delay(1000);
-  send("Hello Slave!");
 }
 
 void loop() {
-  static uint8_t  test=0x55;
-  delay(1000);
-  send((char*)&test);
-  test++;
+  LeftElem(1);
+  for(uint8_t k=0;k<ELEMCNT;k++)
+  {
+    bb[k] = a[ELEMCNT-k-1];
+    }
+  esp.writeData(bb,ELEMCNT);
+  delay(10);
 }
